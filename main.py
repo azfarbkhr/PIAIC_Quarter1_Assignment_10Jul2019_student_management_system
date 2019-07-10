369392395b3f39009aee10e10854319fda617782
azfarbkhr <azfarbkhr@gmail.com>
2019-07-10 10:10:19 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+''' 
+Assignment Name: Student Management System
+prepared by: Syed Azfar Ali
+PIAIC
+'''
+
+# variables used
+menu_options = ("Add New Student", "Print Students List", "Find Student")
